e6c8e1739a4415a7a0ec6561f2d86183beed5fe3
sava1116 <sasasergeevic809@gmail.com>
2025-03-05 12:30:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значения параметра x:");
+            string s=Console.ReadLine();
+            double x=Convert.ToDouble(s);
+            double f = 5 * Math.Cos(x);
+            Console.WriteLine($"Значение F={f].");
+            Console.ReadKey();
+        }
+    }
+}
